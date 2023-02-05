b00aab939bb7d9a28294aa5b204813b281dbf953
James Twose <jamestwose@Mac-mini.home>
2023-02-05 22:22:41 +0100

    working on getting the pearson correlations to work in rust

--- pima/src/pima_eda.rs
+++ pima/src/pima_eda.rs
@@ -1,35 +1,32 @@
-use std::io::Cursor;
-use color_eyre::{Result};
+use ndarray::prelude::*;
+// use ndarray::arr2;
+use ndarray_stats::CorrelationExt;
+use color_eyre::Result;
 use polars::prelude::*;
+use std::io::Cursor;
 // use pl::SeriesMethods;
 use reqwest::blocking::Client;
 
 pub fn run() -> Result<()> {
-    
     println!("Exploratory Data Analysis");
     println!("=========================");
-    
+
     // specify the URL where the csv is located
     let csv_url = "https://raw.githubusercontent.com/jameshtwose/example_deliverables/main/classification_examples/pima_diabetes/diabetes.csv";
-    
+
     // read in the data as a vector using the reqwest Client
-    let data: Vec<u8> = Client::new()
-        .get(csv_url)
-        .send()?
-        .text()?
-        .bytes()
-        .collect();
-    
+    let data: Vec<u8> = Client::new().get(csv_url).send()?.text()?.bytes().collect();
+
     // convert the data to a polars data frame
     let df = CsvReader::new(Cursor::new(data))
         .has_header(true)
         .finish()?
         .lazy()
         .collect()?;
-    
-    // specify the column name of the target 
+
+    // specify the column name of the target
     let target = "Outcome";
-    
+
     // print the head of the data frame (first x rows)
     // the amount of rows and columns
     // and the descriptive statistics of the columns
@@ -39,23 +36,32 @@ pub fn run() -> Result<()> {
     println!("{:?}", df.shape());
     println!("Data Frame description:");
     println!("{:?}", df.describe(None));
+    println!("Value counts of the target:");
+    println!(
+        "{:?}",
+        df.column(target).unwrap().clone().value_counts(true, true)
+    );
 
-    // let s = df.lazy()
-    //         .select([col(target)])
-    //         .collect()?;
-    let s = Series::new(target, [0,1,0,1,0,1,0,1,1,0,1,0,1,0,0,0,0,0,1,1,1,0,0,1]);
-    // let s = Series::new(target, df.select([target]));
+    // calculate the correlation between the target and the other columns
+    let Glucose_BMI_corr = df
+        .select(["Glucose", "BMI"])
+        .unwrap()
+        .to_ndarray::<Float64Type>()?
+        // .unwrap()
+        .pearson_correlation();
 
-    let target_value_counts = s.value_counts(true, true);
-    println!("{:?}", target_value_counts);
+    let tmp = df
+        .select(["Glucose", "BMI"])
+        .unwrap()
+        .to_ndarray::<Float64Type>()?;
 
-    // show the correlations between all of the features of interest
-    // let Glucose_BMI_corr = pearson_corr(df.lazy().select([col("Glucose")]).collect()?, 
-    //                                         df.lazy().select([col("BMI")]).collect()?, 
-    //                                         1);
+    let tmp2 = Array::from_shape_vec((2, 768).f(), vec![tmp])?;
 
-    // df.pearson_corr("Glucose", "BMI", 1);
+    // let a = arr2(&[[1., 3., 5.],
+    //     [2., 4., 6.]]);
+    // let corr = a.pearson_correlation().unwrap();
 
+    println!("{:?}", tmp2);
     // println!("{:?}", Glucose_BMI_corr);
     Ok(())
 }
